1f924e9c30016b9248f4981099e0a400b4651690
Todd Spainhour <toddspainhour@gmail.com>
2020-03-21 23:18:00 -0500

    login button added

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,4 +1,14 @@
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
+
+import auth from './components/auth/auth';
+
 import '../styles/main.scss';
 import 'bootstrap';
 
-console.error('your js is connected');
+const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  auth.loginButton();
+};
+
+init();
