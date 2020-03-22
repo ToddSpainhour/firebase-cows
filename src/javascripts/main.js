8b9f6d3114cd079db38ffd8f3f6531000d389d3c
Todd Spainhour <toddspainhour@gmail.com>
2020-03-22 11:00:34 -0500

    added logout button

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app';
 import apiKeys from './helpers/apiKeys.json';
 
 import authData from './helpers/data/authData';
+import myNavbar from './components/myNavbar/myNavbar';
 import auth from './components/auth/auth';
 
 import '../styles/main.scss';
@@ -11,6 +12,7 @@ const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   authData.checkLoginStatus();
   auth.loginButton();
+  myNavbar.logoutEvent();
 };
 
 init();
