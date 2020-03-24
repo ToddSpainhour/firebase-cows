ac1bde05cbf8a1eb127ed063612991a1f5a5f7a9
Todd Spainhour <toddspainhour@gmail.com>
2020-03-23 21:31:36 -0500

    farmers wip

---
+++ src/javascripts/components/farmhouse/farmhouse.js
@@ -0,0 +1,6 @@
+const buildFarmers = () => {
+  console.error('you made some farmers');
+};
+
+
+export default { buildFarmers };
--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -2,9 +2,11 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 import pasture from '../../components/pasture/pasture';
+import farmhouse from '../../components/farmhouse/farmhouse';
 
 const authDiv = $('#auth');
 const pastureDiv = $('#pasture');
+const farmhouseDiv = $('#farmhouse');
 const logOutButton = $('#navbar-logout-button');
 
 const checkLoginStatus = () => {
@@ -12,8 +14,10 @@ const checkLoginStatus = () => {
     if (user) {
       authDiv.addClass('hide');
       pastureDiv.removeClass('hide');
+      farmhouseDiv.removeClass('hide');
       logOutButton.removeClass('hide');
       pasture.buildCows();
+      farmhouse.buildFarmers();
     } else {
       authDiv.removeClass('hide');
       pastureDiv.addClass('hide');
