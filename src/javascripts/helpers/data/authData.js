41ddd7500d50e3a8bc2d1834236457d3443717ce
Todd Spainhour <toddspainhour@gmail.com>
2020-03-24 17:44:05 -0500

    added single farmer view

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -7,6 +7,7 @@ import farmhouse from '../../components/farmhouse/farmhouse';
 const authDiv = $('#auth');
 const pastureDiv = $('#pasture');
 const farmhouseDiv = $('#farmhouse');
+const singleFarmerDiv = $('#single-farmer');
 const logOutButton = $('#navbar-logout-button');
 
 const checkLoginStatus = () => {
@@ -16,6 +17,7 @@ const checkLoginStatus = () => {
       authDiv.addClass('hide');
       pastureDiv.removeClass('hide');
       farmhouseDiv.removeClass('hide');
+      singleFarmerDiv.removeClass('hide');
       logOutButton.removeClass('hide');
       pasture.buildCows();
       farmhouse.buildFarmers();
@@ -24,6 +26,7 @@ const checkLoginStatus = () => {
       authDiv.removeClass('hide');
       pastureDiv.addClass('hide');
       farmhouseDiv.addClass('hide');
+      singleFarmerDiv.addClass('hide');
       logOutButton.addClass('hide');
     }
   });
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,17 +2,18 @@ import firebase from 'firebase/app';
 import apiKeys from './helpers/apiKeys.json';
 
 import authData from './helpers/data/authData';
-import myNavbar from './components/myNavbar/myNavbar';
 import auth from './components/auth/auth';
+import myNavbar from './components/myNavbar/myNavbar';
 
 import '../styles/main.scss';
-import 'bootstrap';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   authData.checkLoginStatus();
   auth.loginButton();
   myNavbar.logoutEvent();
+  $('body').on('mouseenter', '.farmer-card', (e) => e.target.closest('.card').classList.add('bg-danger'));
+  $('body').on('mouseleave', '.farmer-card', (e) => e.target.closest('.card').classList.remove('bg-danger'));
 };
 
 init();
